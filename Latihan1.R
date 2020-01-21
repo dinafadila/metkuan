f5f9ea20faaa601a5ed5ad9e69fde1d4c3b6d3d4
dina fadila <39585270+dinafadila@users.noreply.github.com>
2020-01-21 10:03:07 +0700

    Add files via upload

---
+++ Latihan1.R
@@ -0,0 +1,14 @@
+data = read.csv("~/Documents/data.csv", header = TRUE,sep = ",")
+View(data)
+data$Tahun
+
+g_range<-range(0, 1000000)
+plot(data$Volume,type="o",col="blue",yli=g_range,axes=FALSE,ann=FALSE)
+axis(1,at=1:9,lab=c("2010","2011","2012","2013","2014","2015","2016","2017","2018"))
+axis(2,las=1,at=1000*c(0 : 1000000))
+box()         #buat bikin box nya
+lines(data$Nilai,type='o',pch=22,lty=2,col="red") #pch : tipe dotnya, lty : tipe garis
+title(main="Volume dan Nilai Produksi Susu Perusahaan Sapi Perah Tahun 2010-2018",col.main="red",font.main=4)
+title(xlab="Tahun",col.lab=rgb(0,0.5,0))
+title(ylab="Jumlah",col.lab=rgb(0,0.5,0))
+legend(1,g_range[2]-1,c("Volume","Nilai"),cex=0.8,col=c("blue","red"),pch=21:22,lty=1:2)
