324286123d497214ee9883658b9a539e53fb53f5
dina fadila <39585270+dinafadila@users.noreply.github.com>
2020-01-21 13:51:50 +0700

    Update Latihan1.R

--- Latihan1.R
+++ Latihan1.R
@@ -12,3 +12,17 @@ title(main="Volume dan Nilai Produksi Susu Perusahaan Sapi Perah Tahun 2010-2018
 title(xlab="Tahun",col.lab=rgb(0,0.5,0))
 title(ylab="Jumlah",col.lab=rgb(0,0.5,0))
 legend(1,g_range[2]-1,c("Volume","Nilai"),cex=0.8,col=c("blue","red"),pch=21:22,lty=1:2)
+
+#nomor 2
+var1 <- data$Volume
+var2 <- data$Nilai
+var3 <- c("2010","2011","2012","2013","2014","2015","2016","2017","2018")
+typeof(var1)
+typeof(var2)
+dataset <- data.frame(var1,var2,var3)
+names(dataset)<-c("Volume", "Nilai", "string")
+dataset$string <- NULL
+barplot(as.matrix(dataset),main = "Volume dan Nilai Produksi Susu Perusahaan Sapi Perah Tahun 2010-2018", xlab="tahun", ylab ="jumlah", beside = TRUE,col = rainbow(9),ylim=c(0,1000000))
+legend("topleft", c("2010","2011","2012","2013","2014","2015","2016","2017","2018"),cex = 0.6, bty ="n",fill=rainbow(9))
+
+
